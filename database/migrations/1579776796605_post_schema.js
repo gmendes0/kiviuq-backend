7fcfea99c5e4bf76af723744b6b35674525c98eb
mendesg0 <gabriel@prestige.com.br>
2020-01-23 08:10:19 -0300

    feat: add post migration, and the relationship with users

---
+++ database/migrations/1579776796605_post_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.text('description')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
