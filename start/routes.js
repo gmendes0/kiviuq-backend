bd2d0a61047852e0bbf649ad4c745f7295cd47bc
gmendes0 <gmendes230@gmail.com>
2020-01-26 13:54:15 -0200

    test: added post tests
    Post tests created;
    Now, Post show route not require authentication,
    
    BREAKING CHANGE: Post show route not require authentication.

--- start/routes.js
+++ start/routes.js
@@ -19,12 +19,12 @@ const Route = use("Route");
 Route.post("/register", "AuthController.register");
 Route.post("/authenticate", "AuthController.authenticate");
 Route.get("/", "PostController.index");
+Route.get("/posts/:id", "PostController.show");
 Route.group(() => {
   /**
    * Post routes
    */
   Route.post("/posts", "PostController.store");
-  Route.get("/posts/:id", "PostController.show");
   Route.put("/posts/:id", "PostController.update");
   Route.delete("/posts/:id", "PostController.destroy");
 
